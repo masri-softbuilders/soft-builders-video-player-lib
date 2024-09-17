9282cba54d3352ca23e93d3d7b9f4d1650218220
Mohammed Masri <masri.softbuilders@gmail.com>
2024-09-17 17:02:42 +0400

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import SoftBuildersVideoPlayer from "./components/SoftBuildersVideoPlayer";
 
+export {
+  SoftBuildersVideoPlayerChapter,
+  SoftBuildersVideoPlayerNote,
+  SoftBuildersVideoPlayerOptions,
+  SoftBuildersVideoPlayerSource,
+} from "./components/SoftBuildersVideoPlayer/types";
+
 export default SoftBuildersVideoPlayer;
